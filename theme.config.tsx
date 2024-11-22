3290bdc067b822b64745200b7e8c22464be9cc72
David Dada <daveproxy80@gmail.com>
2024-11-22 22:53:43 +0100

    Fix: update old github name to the new one

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,12 +5,12 @@ import HeadComponent from "@components/head";
 const config: DocsThemeConfig = {
   logo: <img src="/logo.svg" style={{ height: "60px" }} />,
   project: {
-    link: "https://github.com/starknet-id",
+    link: "https://github.com/lfglabs-dev",
   },
   chat: {
     link: "https://discord.gg/KQkPNymrE8",
   },
-  docsRepositoryBase: "https://github.com/starknet-id/docs-starknet-id",
+  docsRepositoryBase: "https://github.com/lfglabs-dev/docs.starknet.id",
   footer: { component: null },
   useNextSeoProps() {
     return {
