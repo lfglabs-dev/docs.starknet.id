060b05ae0ba0e41132dd54e2475a0e647651ac84
Fricoben <benjamin.flores.pro@gmail.com>
2024-11-25 11:53:02 +0800

    fix: build

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,6 @@ const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   latex: true,
-  flexsearch: {
-    codeblocks: false,
-  },
   defaultShowCopyCode: true,
 });
 
